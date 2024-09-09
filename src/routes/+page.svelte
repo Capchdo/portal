41b2caaddd488da66b51f8e6b8815168b389123c
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2024-09-09 11:40:55 +0800

    feat: 添加转换器的链接

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,27 @@
 </script>
 
 <div class="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-  <h2 class="text-2xl font-bold tracking-tight text-gray-900">Portal</h2>
+  <div class="flex">
+    <h2 class="flex-1 text-2xl font-bold tracking-tight text-gray-900">Portal</h2>
+    <a class="text-gray-900" href="https://webvpn.swo.moe" target="_blank"
+      >转换器
+      <!-- https://heroicons.com arrow-top-right-on-square -->
+      <svg
+        class="inline size-6 align-text-bottom"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+        />
+      </svg>
+    </a>
+  </div>
 
   <fieldset>
     <legend class="font-bold">URL 类型</legend>
